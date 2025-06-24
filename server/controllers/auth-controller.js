c1d850eaec0049cb98a8ae1173b6894ec62f7afb
Soham Bamane <155154551+codeminer29@users.noreply.github.com>
2025-06-24 12:37:50 +0530

    initial

---
+++ server/controllers/auth-controller.js
@@ -0,0 +1,81 @@
+const User = require("../models/user-model");
+const bcrypt = require("bcryptjs")
+
+//
+//
+//      Home Logic
+//
+//
+
+const home = async (req, res) => {
+    try {
+        res.status(200).send("Hello, welcome to codeminer29 using router");
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+//
+//
+//      registration Logic
+//
+//
+
+const register = async (req, res) => {
+    try {
+        console.log(req.body);
+        const { username, email, phone, password } = req.body;
+
+        const userExist = await User.findOne({ email });
+
+        if (userExist) {
+            return res.status(400).json({ message: "Email already exists" });
+        }
+
+        //hash password
+        // const saltRound = 10;
+        // const hash_password = await bcrypt.hash(password, saltRound);
+
+        const userCreated = await User.create({ username, email, phone, password });
+
+        res.status(201).json({ msg: "Registration successful", token: await userCreated.generateToken(), userId: userCreated._id.toString() });
+    } catch (error) {
+        res.status(500).json("internal server error");
+    }
+};
+
+//
+//
+//      Login Logic
+//
+//
+
+const login = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        const userExits = await User.findOne({ email });
+        console.log(userExits);
+
+
+        if (!userExits) {
+            return res.status(400).json({ message: "Invalid Credentials" });
+        }
+
+        // const user = await bcrypt.compare(password, userExits.password);
+
+        const user = await userExits.comparePassword(password);
+
+        if (user) {
+            res.status(200).json({ msg: "Login successful", token: await userExits.generateToken(), userId: userExits._id.toString() });
+        } else {
+            res.status(401).json({ message: "Invalid email or password" })
+        }
+
+    } catch (error) {
+        // res.status(500).json("Internal server error");
+        next(error);
+    }
+}
+
+module.exports = { home, register, login };
---
+++ server/validators/auth-validator.js
@@ -0,0 +1,27 @@
+const { z } = require("zod");
+
+//creating an object schema.
+const signupSchema = z.object({
+    username: z
+        .string({ required_error: "Name is require" })
+        .trim()
+        .min(3, { message: "Name must be atleast 3 characters" })
+        .max(255, { message: "Name must not be more than 255 characters" }),
+    email: z
+        .string({ required_error: "Email is require" })
+        .trim()
+        .email({ message: "Invalid Email address" })
+        .min(3, { message: "Email must be atleast 3 characters" })
+        .max(255, { message: "Email must not be more than 255 characters" }),
+    phone: z
+        .string({ required_error: "Phone is require" })
+        .trim()
+        .min(10, { message: "Phone must be atleast 10 characters" })
+        .max(20, { message: "Phone must not be more than 20 characters" }),
+    password: z
+        .string({ required_error: "Password is require" })
+        .min(7, { message: "Password must be atleast 7 characters" })
+        .max(1024, { message: "Password must not be more than 1024 characters" }),
+});
+
+module.exports = signupSchema;
